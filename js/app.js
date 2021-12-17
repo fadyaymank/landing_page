a67d1ce5bfcf44e957978545ee194be1a6b2d9bf
eduapps <fadyaymank@gmail.com>
2021-12-17 19:05:10 +0200

    finished the landing page project

---
+++ js/app.js
@@ -0,0 +1,83 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+// Build menu
+var numberOfSections = document.getElementsByClassName("landing__container");
+
+// make a loop to build li for navbar
+for (var i = 0; i < numberOfSections.length; i++) {
+  if (i == 0) {
+    const htmlTextToAdd = `<a href="#section${
+      i + 1
+    }" class="btn active" >Section ${i + 1}</a>`;
+    const mainHeading = document.querySelector("#navbar__list");
+    mainHeading.insertAdjacentHTML("afterbegin", htmlTextToAdd);
+  } else {
+    const htmlTextToAdd = `<a href="#section${i + 1}" class="btn" >Section ${
+      i + 1
+    }</a>`;
+
+    const mainHeading = document.querySelector("#navbar__list");
+    mainHeading.insertAdjacentHTML("afterbegin", htmlTextToAdd);
+  }
+}
+
+// Set sections as active
+
+var btns = document.getElementsByClassName("btn");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+const mainHeading2 = document.querySelector(".navbar__menu");
+mainHeading2.style.cssText =
+  "color: black; background-color: white; font-size: 30px";
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+
+// Add class 'active' to section when near top of viewport
+
+// Scroll to anchor ID using scrollTO event
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
